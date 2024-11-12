90ada387901d9a16fce2e3de78ccefc315fc27bc
harisangar2004 <122169178+harisangar2004@users.noreply.github.com>
2024-11-12 14:11:39 +0530

    Create 4output.c

---
+++ 4output.c
@@ -0,0 +1,27 @@
+#include<stdio.h>                                                                                                                                                       
+void main() {                                                                                                                                                           
+int var1;                                                                                                                                                               
+var1 = 10;                                                                                                                                                              
+printf("%d", var1);                                                                                                                                                     
+printf("malai");                                                                                                                                                        
+for(int i=0;i<10;i=i+1){                                                                                                                                                
+        printf("%d", i);                                                                                                                                                
+}                                                                                                                                                                       
+if(i < 5){                                                                                                                                                              
+        printf("hello");                                                                                                                                                
+} else {                                                                                                                                                                
+        printf("world");                                                                                                                                                
+}                                                                                                                                                                       
+while(i<5){                                                                                                                                                             
+        printf("%d", i);                                                                                                                                                
+                                                                                                                                                                        
+i=i+2;                                                                                                                                                                  
+}int add(int a, int b) {                                                                                                                                                
+         int result;                                                                                                                                                    
+                                                                                                                                                                        
+for(int i=0;i<10;i=i+2){                                                                                                                                                
+        printf("%d", i);                                                                                                                                                
+}                                                                                                                                                                       
+         return i;                                                                                                                                                      
+ }                                                                                                                                                                      
+}       
